3dfc54f9c48997658cf8d5fca9ee69e2834c59e7
mandy-shen <shen.mo@northeastern.edu>
2021-03-19 10:40:59 -0700

    solved 127.0.0.1 sandbox with using CommandLineRunner(removed)

--- src/main/java/com/mandy/wordcount/WordcountApplication.java
+++ src/main/java/com/mandy/wordcount/WordcountApplication.java
@@ -1,34 +1,37 @@
 package com.mandy.wordcount;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.IOException;
 import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 
-@SpringBootApplication(scanBasePackages =  { "rest" })
-public class WordcountApplication implements CommandLineRunner {
+@SpringBootApplication(scanBasePackages = {"rest"})
+public class WordcountApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WordcountApplication.class, args);
+        run();
     }
 
-    @Override
-    public void run(String... args) throws IOException {
-        InetAddress localhost = InetAddress.getLocalHost();
-        String ip = String.format("%s %s\n", localhost.getHostAddress(), localhost.getHostName());
-        System.out.println(ip);
+    public static void run() {
+        try {
+            InetAddress localhost = InetAddress.getLocalHost();
+            String ip = String.format("%s,%s,%s\n", System.getenv("id"),
+                    localhost.getHostAddress(), localhost.getHostName());
+            System.out.println(ip);
 
-        Path dirPath = Paths.get("/volume");
-        if (!Files.exists(dirPath))
-            Files.createDirectories(dirPath);
-        Files.write(Paths.get("/volume/hosts"), ip.getBytes(), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+            Path dirPath = Paths.get("/volume");
+            if (!Files.exists(dirPath))
+                Files.createDirectories(dirPath);
+            Files.write(Paths.get("/volume/hosts"), ip.getBytes(), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
