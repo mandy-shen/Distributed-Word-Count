6de23f279c4294e076e38a3c6cca05e7470ff029
mandy-shen <shen.mo@northeastern.edu>
2021-03-19 10:11:01 -0700

    add public volume >> add host_ips

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,5 @@ RUN apt-get update && apt-get install -y maven
 COPY . /project
 RUN  cd /project && mvn clean package
 
-RUN echo "hello world" > /tmp/apps
-
 #run the spring boot application
 ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom", "-Dblabla", "-jar","/project/target/wordcount-1.0.0.jar"]
--- src/main/java/com/mandy/wordcount/WordcountApplication.java
+++ src/main/java/com/mandy/wordcount/WordcountApplication.java
@@ -1,13 +1,34 @@
 package com.mandy.wordcount;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages =  { "rest"})
-public class WordcountApplication {
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+@SpringBootApplication(scanBasePackages =  { "rest" })
+public class WordcountApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(WordcountApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws IOException {
+        InetAddress localhost = InetAddress.getLocalHost();
+        String ip = String.format("%s %s\n", localhost.getHostAddress(), localhost.getHostName());
+        System.out.println(ip);
+
+        Path dirPath = Paths.get("/volume");
+        if (!Files.exists(dirPath))
+            Files.createDirectories(dirPath);
+        Files.write(Paths.get("/volume/hosts"), ip.getBytes(), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
     }
 
 }
